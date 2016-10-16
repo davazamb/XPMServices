691f946187006f7611556c34f5500e0a50b43880
David Zambrano <davazamb10@outlook.com>
2016-10-16 11:57:04 -0400

    Classes, Interfaces, Infrastructure

---
+++ XPMServices/XPMServices/ViewModels/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPMServices.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+
+        public MainViewModel()
+        {
+            LoadMenu();
+        }
+
+        private void LoadMenu()
+        {
+            Menu = new ObservableCollection<MenuItemViewModel>();
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_orders.png",
+                PageName = "ProductPage",
+                Title = "Productos",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_client.png",
+                PageName = "ConsultPage",
+                Title = "Consultas",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_settings.png",
+                PageName = "SettingsPage",
+                Title = "Ajustes",
+            });
+        }
+    }
+
+}
